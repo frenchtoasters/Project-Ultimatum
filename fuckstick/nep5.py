3fd04c68379733d4e51ce14417206e455f14fc76
thatguy <icowhalegithube@gmail.com>
2018-05-17 22:01:30 -0400

    updates to the nep5 token to have additional functions, does this go there?

--- fuckstick/nep5.py
+++ fuckstick/nep5.py
@@ -44,6 +44,107 @@ def handle_nep51(ctx, operation, args):
         if len(args) == 2:
             return do_allowance(ctx, args[0], args[1])
 
+    '''
+    Create_Update_Pool
+    Assumption: operator_msg will be like the password to the pool, it basically needs to be a signed message proving that the owner is who they say. Therefore they would need to sign the message with a private key that is theirs and theirs only. 
+        Args:
+            - operator_key == operator public key
+            - operator_msg == signed message by owner
+            - size
+            - min
+            - max
+            - current
+        Return:
+            pool_id = init_random()
+            find_pool(pool_id) ? False: current = 0
+            - True ? operator_key 
+            - False ? !operatror_key
+     '''
+    elif operation == 'startPool':
+        if len(args) == 4:
+            return start_pool(ctx, args[0], args[1], args[2], args[3])
+    
+    '''
+    Deposit_Pool
+        Args:
+            - pool_id
+            - size
+        Return:
+            max = Find_Pool(pool_id)
+            - True ? size <= max
+            - False ? size > max
+     '''
+    elif operation == 'depositPool':
+        if len(args) == 2:
+            return deposit_pool(ctx, args[0], args[1])
+
+    '''
+    Find_Pool
+        Args:
+            - pool_id
+        Return:
+            pool = Storage.get(pool_id)
+            - True ? pool
+            - False ? !pool
+     '''
+    elif operation == 'findPool':
+        if len(args) == 1:
+            return find_pool(ctx, args[0])
+
+    '''
+    Destory_Complete_Pool
+        Args:
+            - operator_msg
+            - address
+        Return: 
+            - True ? Transfer
+            - False ? !Transfer
+    '''
+    elif operation == 'finishPool':
+        if len(args) == 2:
+            return finish_pool(ctx, args[0], args[1])
+
+    '''
+    WorkFlow Functions:
+    Validate_Op_key:
+        Args:
+            - operator_key
+            - pool_id
+        Return:
+            pool = Storage.get(pool_id)
+            valid = decryptMsg(pool,operator_key)
+            - True ? valid
+            - False ? !valid
+
+    Validate_Msg:
+        Args:
+            - pool_id
+            - operator_msg
+        Return:
+            pool = find_pool(pool_id)
+            - True ? get_message(pool['operator_key'],operator_msg)
+            - False ? !get_message(pool['operator_key'],operator_msg)
+
+    Get_Message:
+        Args:
+            - operator_key
+            - operator_msg
+        Return:
+            NOTE: decrypt_message is just saying to do a normal check if a message was signed by the same private key
+            msg = decrypt_message(operator_key,operator_msg)
+            - True ? decrypt_message == valid_operation(msg)
+            - False ? !decrypt_message == valid_operation(msg)
+
+    Check_Max_Pool:
+        Args:
+            - pool_id
+            - size
+        Return:
+            pool = find_pool(pool_id)
+            - True ? pool_id['max'] > (pool_id['current'] + size)
+            - False ? pool_id['max'] <= (pool_id['current'] + size)
+    '''
+
     return False
 
 
